e80a23fe0d5f1986b350a9483aa520bd45751667
Tamunokuro <tamunokrobluejack@gmail.com>
2023-02-01 10:02:10 +0100

    add species column

--- queries.sql
+++ queries.sql
@@ -4,8 +4,43 @@ SELECT * from animals WHERE name LIKE '%mon';
 SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
 SELECT name from animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth from animals WHERE name = 'Agumon' OR name = 'Pikachu';
-SELECT (name, escape_attempts ) FROM animals WHERE weight_kg > 10.5;
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * from animals WHERE neutered = true;
 SELECT * from animals WHERE name != 'Gabumon';
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'unspecified';
+SELECT * from animals WHERE species = 'unspecified';
+ROLLBACK TRANSACTION;
+SELECT * from animals
+
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+SELECT * from animals;
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+
+SELECT * from animals;
+
+BEGIN TRANSACTION;
+DELETE from animals;
+SELECT * from animals;
+ROLLBACK TRANSACTION;
+
+BEGIN TRANSACTION;
+DELETE from animals WHERE date_of_birth > '2020-01-01';
+SAVEPOINT deleteDOB20200101;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO deleteDOB20200101;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT TRANSACTION;
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) from animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) from animals;
+SELECT neutered, MAX(escape_attempts) from animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) from animals GROUP BY species;
+SELECT species, AVG(escape_attempts) from animals 
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species; 
--- schema.sql
+++ schema.sql
@@ -1,10 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE NOT NULL,
     escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL, 
     weight_kg DECIMAL NOT NULL,
 );
+
+ALTER Table animals
+ADD species VARCHAR(20);
+
